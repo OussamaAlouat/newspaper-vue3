8b68645d7329bfef3f4a3c448de7f3b9259552bf
OussamaAlouat <al317257@uji.es>
2023-04-30 01:19:58 +0200

    FEAT: used search component

--- src/views/NewsPaperList.vue
+++ src/views/NewsPaperList.vue
@@ -1,14 +1,16 @@
 <script lang="ts" setup>
-import useNewspaper from '@/composables/useNewsPapers';
+import useNewspapers from '@/composables/useNewsPapers';
 import NewsPaperList from '@/components/NewsPaperList.vue';
+import NewsPaperSearch from '@/components/NewsPaperSearch.vue';
 
-const { news, isLoading } = useNewspaper();
+const { news, searchedNews ,isLoading } = useNewspapers();
 </script>
 
 <template>
+  <news-paper-search></news-paper-search>
   <h3 v-if="isLoading">Is loading</h3>
-  <div v-if="news && !isLoading">
-    <news-paper-list :newsPapers="news"></news-paper-list>
+  <div v-if="searchedNews && !isLoading">
+    <news-paper-list :newsPapers="searchedNews"></news-paper-list>
   </div>
 </template>
 
