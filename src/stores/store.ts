54409e30efe6bf18872fa60c4a08fa7e9a046d20
OussamaAlouat <al317257@uji.es>
2023-04-30 01:18:40 +0200

    FEAT: add functions and new properties on store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -5,17 +5,25 @@ import type { NewsPaper } from '@/interfaces/newsPaper';
 
 export const useNewsPaperStore = defineStore('news', () => {
   const news = ref<NewsPaper[]>([]);
+  const searchedNews = ref<NewsPaper[]>([]);
+
   const setNewsPapers= (data: NewsPaper[]) => {
     news.value = data;
+  }
+
+  const setSearchedNews = (data: NewsPaper[]) => {
+    searchedNews.value = data;
   }
 
   return { 
     // Properties
     news,
+    searchedNews,
 
     // Getters
 
     // Actions
     setNewsPapers,
+    setSearchedNews,
    }
 })
