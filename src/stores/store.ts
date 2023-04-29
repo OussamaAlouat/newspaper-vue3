990d4758b8d3db9aba1b267b2ce74a31456831b7
OussamaAlouat <al317257@uji.es>
2023-04-29 20:00:33 +0200

    FEAT: added functionality to store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,14 +1,21 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+import type { NewsPaper } from '@/interfaces/newsPaper';
 
 export const useNewsPaperStore = defineStore('news', () => {
-
+  const news = ref<NewsPaper[]>([]);
+  const setNewsPapers= (data: NewsPaper[]) => {
+    news.value = data;
+  }
 
   return { 
     // Properties
+    news,
 
     // Getters
 
     // Actions
+    setNewsPapers,
    }
 })
