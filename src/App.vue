fa08848e1528dcd0503709e6e05fa93e111f3f44
OussamaAlouat <al317257@uji.es>
2023-04-30 01:18:04 +0200

    FIX: fixed some css

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <h2>News app</h2>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  text-align: center;
+  justify-content: center;
+}
+</style>
