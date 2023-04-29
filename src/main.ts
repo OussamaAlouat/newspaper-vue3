9a0e6f00abeb12d32f4012adff7b5c0a3dff1745
OussamaAlouat <al317257@uji.es>
2023-04-29 21:09:28 +0200

    FEAT: add vuetify to app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+// Vuetify
+import 'vuetify/styles';
+import "@mdi/font/css/materialdesignicons.css";
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue';
 import router from './router';
@@ -8,7 +15,12 @@ import router from './router';
 import './assets/main.css';
 
 const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives
+});
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
