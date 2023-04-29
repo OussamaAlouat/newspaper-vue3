e0d445eb7b966b723776bcff19efea024c253e56
OussamaAlouat <al317257@uji.es>
2023-04-29 18:41:04 +0200

    FEAT: added configuration for cache

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+
+/*
+  Configuration of cache, for all queries,
+  we will mantein the data for only one minute
+*/
+
+VueQueryPlugin.install(app, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        cacheTime: 1000 * 60, 
+      }
+    }
+  }
+});
 
 app.mount('#app')
