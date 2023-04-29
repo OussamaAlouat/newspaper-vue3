a0d7074464f45ee0a5582651039b4161f2a5d7c6
OussamaAlouat <al317257@uji.es>
2023-04-29 20:26:38 +0200

    FEAT: first implementation of list news

---
+++ src/components/NewsPaperList.vue
@@ -0,0 +1,24 @@
+
+<script lang="ts" setup>
+import type { NewsPaper } from '@/interfaces/newsPaper';
+import { toRef } from 'vue';
+
+  const props = defineProps<{ newsPapers: NewsPaper[] }>();
+  const news = toRef(props, 'newsPapers');
+</script>
+
+<template>
+  <h4>Listado de noticias</h4>
+  <div v-for="npaper of news" :key="npaper._id">
+    <div>Title:{{ npaper.title }}</div>
+    <textarea name="new" id="new" cols="30" rows="10" :value="npaper.abstract"></textarea>
+    <div> Created on:  {{ npaper.creation_date }}</div>
+    <div> Publisher:  {{ npaper.publisher.name }}</div>
+  </div>
+</template>
+
+
+<style lang="scss">
+
+
+</style>
--- src/views/NewsPaperList.vue
+++ src/views/NewsPaperList.vue
@@ -1,14 +1,16 @@
+<script lang="ts" setup>
+import useNewspaper from '@/composables/useNewsPapers';
+import NewsPaperList from '@/components/NewsPaperList.vue';
+
+const { news, isLoading } = useNewspaper();
+</script>
+
 <template>
-  <div>
-    List of NewsPapers
+  <h3 v-if="isLoading">Is loading</h3>
+  <div v-if="news && !isLoading">
+    <news-paper-list :newsPapers="news"></news-paper-list>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
 <style lang="scss">
-
-
 </style>
