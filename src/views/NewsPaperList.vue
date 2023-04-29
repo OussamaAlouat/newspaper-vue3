7644821e506220faac8b8914f80f9a8f695dce92
OussamaAlouat <al317257@uji.es>
2023-04-30 01:47:05 +0200

    FIX: used no data component

--- src/views/NewsPaperList.vue
+++ src/views/NewsPaperList.vue
@@ -2,17 +2,31 @@
 import useNewspapers from '@/composables/useNewsPapers';
 import NewsPaperList from '@/components/NewsPaperList.vue';
 import NewsPaperSearch from '@/components/NewsPaperSearch.vue';
-
+import NoData from '@/components/NoData.vue';
 const { news, searchedNews ,isLoading } = useNewspapers();
 </script>
 
 <template>
   <news-paper-search></news-paper-search>
-  <h3 v-if="isLoading">Is loading</h3>
-  <div v-if="searchedNews && !isLoading">
-    <news-paper-list :newsPapers="searchedNews"></news-paper-list>
+  <div v-if="isLoading">
+
   </div>
+  <div  v-else-if="searchedNews.length === 0" class="no-data">
+    <no-data />
+  </div>
+  <div v-else>
+      <news-paper-list :newsPapers="searchedNews"></news-paper-list>
+  </div>
+
+  <h3 v-if="isLoading">Is loading</h3>
+
 </template>
 
 <style lang="scss">
+.no-data{
+  display: flex;
+  justify-content: center;
+  height: 80%;
+  align-items: center;
+}
 </style>
