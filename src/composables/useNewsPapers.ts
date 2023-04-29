eb91e2886f1e16a4583a76860d764a742101d0a0
OussamaAlouat <al317257@uji.es>
2023-04-29 20:01:18 +0200

    FIX: use useQuery with store

--- src/composables/useNewsPapers.ts
+++ src/composables/useNewsPapers.ts
@@ -1,7 +1,10 @@
+import { useNewsPaperStore } from './../stores/store';
 import { useQuery } from "@tanstack/vue-query";
 import type { AxiosResponse } from "axios";
 
 import newsApi from "@/api/news.api";
+import { storeToRefs } from 'pinia';
+import { watch } from 'vue';
 
 const loadNewsPapers = async():Promise<AxiosResponse> => {
   const data = await newsApi.getAllNewsPapers();
@@ -9,14 +12,26 @@ const loadNewsPapers = async():Promise<AxiosResponse> => {
   return data;
 }
 
+
+
 const useNewspaper = () => {
+  const store = useNewsPaperStore();
+  const { news } = storeToRefs(store);
+
   const { data, isLoading } = useQuery(
     ['newsPaper'],
-    () => loadNewsPapers(),
+    () => loadNewsPapers()
   );
 
+  watch(data, newsPapers => {
+    if(newsPapers && newsPapers.data && newsPapers.data.data ) {
+      const values = newsPapers.data.data;
+      store.setNewsPapers(values)
+    }
+  });
+
   return {
-    news: data,
+    news,
     isLoading
   }
 };
