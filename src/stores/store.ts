9d6409bb9f849a6162ef231dac6a519f038be144
OussamaAlouat <al317257@uji.es>
2023-04-29 18:40:46 +0200

    FIX: change the store

---
+++ src/stores/store.ts
@@ -0,0 +1,14 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useNewsPaperStore = defineStore('news', () => {
+
+
+  return { 
+    // Properties
+
+    // Getters
+
+    // Actions
+   }
+})
