aa2d1041025e1b5ac6b7021fb237b7835002b65d
OussamaAlouat <al317257@uji.es>
2023-04-30 19:04:28 +0200

    FEAT: add newspaper detail component - added the component and composable and view

---
+++ src/views/NewsPaperDetail.vue
@@ -0,0 +1,24 @@
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import useNewsPapeDetail from '@/composables/useNewsPaperDetail';
+import NewsPaperDetail from '@/components/NewsPaperDetail.vue';
+
+const route = useRoute();
+const id = route.params['id'];
+
+const { newsPaper, isLoading } = useNewsPapeDetail(id as string);
+
+</script>
+
+<template>
+  <div v-if="newsPaper">
+    <news-paper-detail :newspaper="newsPaper" />
+  </div>
+  <div v-if="isLoading">
+    Loading
+  </div>
+</template>
+
+
+<style lang="scss"></style>
