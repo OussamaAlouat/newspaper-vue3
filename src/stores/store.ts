fbbdee94b868db512f3d205965530a1b004e4544
OussamaAlouat <al317257@uji.es>
2023-04-30 20:17:06 +0200

    FIX: removed not nedded import

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 import type { NewsPaper } from '@/interfaces/newsPaper';
@@ -23,7 +23,6 @@ export const useNewsPaperStore = defineStore('news', () => {
 
     return data;
   }
-
 
   const setLoadingValue = (value: boolean):void => {
     isLoading.value = value;
