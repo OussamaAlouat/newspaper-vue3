a2e6e80d81ffa339fdc48551ee8f366909226543
OussamaAlouat <al317257@uji.es>
2023-04-30 19:05:54 +0200

    FEAT: add route of detail newspaper

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import NewsPaperList from '@/views/NewsPaperList.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import NewsPaperList from '@/views/NewsPaperList.vue';
+import NewsPaperDetail from '@/views/NewsPaperDetail.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'NewsPaper',
       component: NewsPaperList
+    },
+    {
+      path: '/:id',
+      name: 'NewsPaperDetail',
+      component: NewsPaperDetail
     }
   ]
 })
